e9bbc831dcc44b5074b970ebee1958244d49b861
Kalyani Potu <kalyani.potu@gmail.com>
2024-02-17 19:25:32 +0530

    changed code

--- Assignments/12_02_Task1.py
+++ Assignments/12_02_Task1.py
@@ -1,13 +1,31 @@
 def palindrome(str1):
     str2 = ""
-    n = len(str1) - 1
-    for i in range(len(str1)):
-        str2 = str2 + str1[n - i]
+    for i in str1:
+        str2 = i + str2
     if str2 == str1:
         print(f"{str1} is a palindrome")
     else:
         print(f"{str1} is not a palindrome")
 
 
-input_str = input("Enter the string:")
-palindrome(input_str)
+str = input("Enter the string:")
+in_str = str.lower()
+palindrome(in_str)
+
+# using string index
+'''
+def palindrome(str1):
+    str2 = ""
+    n= len(str1)-1
+    for i in range(len(str1)):
+        str2 = str2 + str1[n-i]
+    if str2 == str1 :
+        print(f"{str1} is a palindrome")
+    else :
+        print(f"{str1} is not a palindrome")
+
+
+str = input("Enter the string :")
+in_str = str.lower()
+palindrome(in_str)
+'''
