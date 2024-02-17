0720440b08e2904d9f23e1ccc88dce7e1dae6969
Kalyani Potu <kalyani.potu@gmail.com>
2024-02-17 16:33:53 +0530

    added files

---
+++ 09_Feb/for loop.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    print(i, end=" ")
+print('\n')
+for i in range(1,5):
+    print(i, end=" ")
+print('\n')
+for i in range(4,5):
+    print(i, end=" ")
+print('\n')
+for i in range(1,5,2):
+    print(i, end=" ")
+print('\n')
+for i in range(-1,5):
+    print(i, end=" ")
+print('\n')
+for i in range(1,5,-1):
+    print(i, end=" ")
+print('\n')
+for i in range(1,-10,):
+    print(i, end=" ")
---
+++ 09_Feb/range.py
@@ -0,0 +1,4 @@
+x = range(5)
+print(x)
+my_list = list(range(5))
+print(my_list)
