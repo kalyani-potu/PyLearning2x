325d47cd4c778b7cf226fa702921e20a1fe53354
Kalyani Potu <kalyani.potu@gmail.com>
2024-02-17 16:41:42 +0530

    added files

--- 07_Feb/is_operator.py
+++ 07_Feb/is_operator.py
@@ -1,17 +1,16 @@
-a=5
-b=5
-c=10
+a = 5
+b = 5
+c = 10
 list1 = []
-list2 =[]
+list2 = []
 list3 = list1
-print(a==b)
-print(a==c)
-print(a is b)
-print(a is c)
-print(list1==list2)
-print(list1 is list2)
-print(list3 is list1)
-list3 = list3 +list1
-print(list3 is list1)
+print('1 : ', a == b)  # numbering just for order
+print('2 : ', a == c)
+print('3 : ', a is b)
+print('4 : ', a is c)
+print('5 : ', list1 == list2)
+print('6 : ', list1 is list2)
+print('7 : ', list3 is list1)
+list3 = list3 + list1
+print('8 : ', list3 is list1)
 print(list3)
-
