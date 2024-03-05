fb7832532b77fb645ed83b4a35f095570b691fe8
Kalyani Potu <kalyani.potu@gmail.com>
2024-03-05 14:37:55 +0530

    Added files

--- 14_Feb/map3.py
+++ 14_Feb/map3.py
@@ -1,8 +1,11 @@
 import math
-
 def cube(num):
     return math.pow(num,3)
 
 numbers = [5,3,9]
 cube_list = list(map(cube, numbers))
 print(cube_list)
+
+######### Using Lambda expression
+cube_list1 = (map(lambda num : math.pow(num,3), numbers))
+print(list(cube_list1))
