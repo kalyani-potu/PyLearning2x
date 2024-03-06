c7dd72231fb784295456d39cd3760045ea0f68f7
Kalyani Potu <kalyani.potu@gmail.com>
2024-03-06 15:27:44 +0530

    Added files

---
+++ 12_Feb/MatchCase.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number : "))
+match number:
+    case 1 :
+        print("You have entered 1")
+    case 2:
+        print("You have entered 2")
+    case _ :
+        print("No idea")
