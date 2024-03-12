742e0d346c1be7fa9e863c8514dd167a58b84c3f
Kalyani Potu <kalyani.potu@gmail.com>
2024-03-12 11:22:02 +0530

    Added files

--- Assignments/09_02_Task2.py
+++ Assignments/09_02_Task2.py
@@ -4,7 +4,8 @@ c = float(input('Enter third side :'))
 if a > 0 and b > 0 and c > 0:
     if a == b == c:
         print('Equilateral Triangle')
-    elif a == b != c or a != b == c or a == c != b:
+    #elif a == b != c or a != b == c or a == c != b:
+    elif a == b or b == c or a == c:
         print('Isosceles Triangle')
     else:
         print('Scalene Triangle')
