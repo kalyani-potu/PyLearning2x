47fd5fa01551babfa774d678e524596081b65fa6
Kalyani Potu <kalyani.potu@gmail.com>
2024-03-23 15:58:13 +0530

    Added files

--- Assignments/Fizzbuzz.py
+++ Assignments/Fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in range(1,21):
-    if i%3 == 0 and i%5 ==0:
+for i in range(1, 21):
+    if i % 3 == 0 and i % 5 == 0:
         print('FizzBuzz')
-    elif i%5 == 0:
+    elif i % 5 == 0:
         print('Buzz')
-    elif i%3 == 0:
+    elif i % 3 == 0:
         print('Fizz')
-    else :
+    else:
         print(i)
