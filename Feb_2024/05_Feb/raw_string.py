c2d37cc286b5e8d8f80ea649db5f1d5b3e45c61f
Kalyani Potu <kalyani.potu@gmail.com>
2024-05-24 08:32:51 +0530

    changed files

--- Feb_2024/05_Feb/raw_string.py
+++ Feb_2024/05_Feb/raw_string.py
@@ -1,5 +1,7 @@
 dir = 'c:\namedir\drive' #here \n is considered as new line
 print(dir)
+dirr = 'c:\namedir\arive' #here \a is considered as some other special character
+print(dirr)
 
 dir1 = r'c:\namedir\drive' #raw string - r is used to assign the value as it is even when the given string has special characters
 print(dir1)
--- Feb_2024/05_Feb/str_format.py
+++ Feb_2024/05_Feb/str_format.py
@@ -4,3 +4,10 @@ print("My name is {name}, age is {age}")
 print(f"My name is {name}, age is {age}")  # f prints the variable value when variable name is given in print()
 print("My name is {}, age is {}".format(name, age))
 print("My name is %s, age is %d" % (name, age))
+
+str_num = "12"
+#str_num = "12a" #gives error as it contains character
+int_num = int(str_num)
+print(type(int_num))
+print(int_num)
+print(str_num)
